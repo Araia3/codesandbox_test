a09d520df6e2af6d3b7eb21fd00976f5d8129ff9
Araia3 <97326430+Araia3@users.noreply.github.com>
2022-02-23 10:25:58 +0900

    codesandboxerr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+
+  //divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグを生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  //divタグの子要素に各要素を設定する
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了のリストに追加していく
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
